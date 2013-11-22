1d660e998535bab21bafca41f22a1352873dc56a
Fernando <ffreitasalves@gmail.com>
2013-11-22 17:17:56 -0200

    criando a view de lista de desejos

--- core/views.py
+++ core/views.py
@@ -48,3 +48,8 @@ def website_home(request):
     return render(request, "website/index.html",
         locals()
     )
+
+def listar_desejos(request):
+    return render(request, "system/list_desejos.html",
+        locals()
+    )
