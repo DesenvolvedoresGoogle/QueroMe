772126946ae309195e5b7774041fe9d631ad9193
Fernando <ffreitasalves@gmail.com>
2013-11-23 04:12:46 -0200

    arrumando a posição do search_indexes

--- core/Bid/search_indexes.py
+++
@@ -1,9 +0,0 @@
-# bid.search_indexes
-import search
-from search.core import porter_stemmer
-from core.models import Bid
-
-# index used to retrieve posts using the title, content or the
-# category.
-search.register(Bid, ('product',),
-    indexer=porter_stemmer)
